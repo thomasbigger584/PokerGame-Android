a2b1db674a55acc4843b316e7bd30605e1a75174
Thomas Bigger <thomasbigger584@gmail.com>
2020-04-05 14:30:35 +0100

    buttons colored

--- app/src/main/java/com/twb/poker/util/SleepUtil.java
+++ app/src/main/java/com/twb/poker/util/SleepUtil.java
@@ -2,7 +2,7 @@ package com.twb.poker.util;
 
 public class SleepUtil {
     public static final double PLAYER_RESPONSE_LOOP_IN_SECONDS = 0.1d;
-    private static final int MULTIPLIER = 10;
+    private static final int MULTIPLIER = 1;
     private static final double ROUND_DELAY_IN_MS = 3 * 1000;
     private static final double GAME_DELAY_IN_MS = 5 * 1000;
     private static final double DEAL_TIME_IN_MS = 1.25 * 1000;
